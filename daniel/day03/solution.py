dcc7d5b0d881c9b86870fe303a6ced2abbccf82a
danielcagostinho <danielcagostinho@gmail.com>
2025-12-03 11:43:11 -0500

    Implement battery joltage calculation for day 03
    Added logic for part 1 and part 2 to calculate the sum of maximum battery joltages from input banks. Helper functions were introduced to find the highest joltage indices, and the main script now prints only the result for part 2.

--- daniel/day03/solution.py
+++ daniel/day03/solution.py
@@ -1,17 +1,63 @@
-def read_input(filename='input.txt'):
+def read_input(filename="input.txt"):
     with open(filename) as f:
         return f.read().strip()
 
 
-def part1(data):
-    pass
+BATTERIES_TO_TURN_ON = 12
+
+
+def calculate_max_joltage_index(bank: str) -> int:
+    largest_joltage_index = 0
+    print(f"parsing sub-bank: {bank}, length: {len(bank)}")
+    for i in range(1, len(bank)):
+        if int(bank[i]) > int(bank[largest_joltage_index]):
+            largest_joltage_index = i
+    return largest_joltage_index
+
+
+def calculate_max_joltage_index_part_2(bank: str, remaining_batteries: int) -> int:
+    largest_joltage_index = 0
+    for i in range(1, len(bank)):
+        if (
+            int(bank[i]) > int(bank[largest_joltage_index])
+            and len(bank) - i >= remaining_batteries
+        ):
+            largest_joltage_index = i
+    return largest_joltage_index
+
+
+def part1(data: str) -> int:
+    max_joltages = []
+    for bank in data.split("\n"):
+        max_joltage_a = calculate_max_joltage_index(bank[: len(bank) - 1])
+        substring = bank[max_joltage_a + 1 :]
+        max_joltage_b = calculate_max_joltage_index(substring)
+        max_joltage = bank[max_joltage_a] + bank[max_joltage_a + 1 + max_joltage_b]
+        max_joltages.append(int(max_joltage))
+    return sum(max_joltages)
 
 
 def part2(data):
-    pass
+    max_joltages = []
+    for bank in data.split("\n"):
+        highest_first_index = calculate_max_joltage_index_part_2(
+            bank[: len(bank) - 1], BATTERIES_TO_TURN_ON - 1
+        )
+        max_joltage_str = bank[highest_first_index]
+        pointer = highest_first_index
+        for i in range(BATTERIES_TO_TURN_ON - 1):
+            sub_bank = bank[pointer + 1 :]
+            next_max_joltage_index = (
+                calculate_max_joltage_index_part_2(sub_bank, BATTERIES_TO_TURN_ON - i - 1) + pointer + 1
+            )
+            max_joltage_str = max_joltage_str + bank[next_max_joltage_index]
+            pointer = next_max_joltage_index
+        max_joltages.append(int(max_joltage_str))
+
+    return sum(max_joltages)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     data = read_input()
-    print(f"Part 1: {part1(data)}")
+    # print(f"Part 1: {part1(data)}")
     print(f"Part 2: {part2(data)}")
